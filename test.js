e3f56f888108a1afb5a5c6b6fde8671ebf8bd569
shawnzhesun <shawn.sun@c3iot.com>
2016-12-29 15:25:59 -0800

    test commit 1

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-
+ 
 console.log('Message on master branch was deleted, and this line was added in branch: topic/engie-shawnzhesun')
 console.log('This line is added in branch: topic/engie-shawnzhesun')
 console.log('This line is also added in branch: topic/engie-shawnzhesun')
