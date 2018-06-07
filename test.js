b2863bb0265afce391476e6d60fd6c4fefd118b0
Shawn Sun <shawn.sun@c3iot.com>
2018-06-07 15:18:06 -0700

    Update test.js

--- test.js
+++ test.js
@@ -4,4 +4,4 @@ console.log('This line is added in branch: topic/engie-shawnzhesun')
 console.log('This line is also added in branch: topic/engie-shawnzhesun')
 console.log('This is a test message')
 //modified in master
-//resolved conflicts in topic/engie-shawnzhesun
+//resolved conflicts in topic/engie-shawnzhesun 
